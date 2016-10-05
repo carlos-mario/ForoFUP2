72dd36e6397ddf9c2e0d1bbc470db8b64e7f7123
luis munoz <luchof756@gmail.com>
2016-10-05 10:42:12 +0200

    add method figuras to develop

--- Parcial/lib/routes.js
+++ Parcial/lib/routes.js
@@ -11,3 +11,10 @@ FlowRouter.route('/vehiculo',{
 		BlazeLayout.render('MainLayout', {main: 'Vehiculo'});
 	}
 });
+
+FlowRouter.route('/figuras',{
+	name: 'figuras',
+	action(){
+		BlazeLayout.render('MainLayout', {main: 'Figuras'});
+	}
+});
