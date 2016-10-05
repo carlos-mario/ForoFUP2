ea20503a3789ab4f1e725e082cbb7c3f91414b40
RodrigoNigua <rodrigoojeda@hotmail.es>
2016-10-05 10:10:46 -0500

    update  libros

--- Parcial/lib/routes.js
+++ Parcial/lib/routes.js
@@ -11,9 +11,17 @@ FlowRouter.route('/vehiculo',{
 		BlazeLayout.render('MainLayout', {main: 'Vehiculo'});
 	}
 });
+<<<<<<< HEAD
 FlowRouter.route('/libro',{
 	name: 'libro',
 	action(){
 		BlazeLayout.render('MainLayout', {main: 'Libro'});
+=======
+
+FlowRouter.route('/deportes',{
+	name: 'deportes',
+	action(){
+		BlazeLayout.render('MainLayout', {main: 'Deportes'});
+>>>>>>> 5ddeb96356d93fa05262f62f42aff08013808143
 	}
 });
