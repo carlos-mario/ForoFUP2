fb14379db55a96f581e206e1d3e1969ad68c2fa9
carlos-mario <carlosluna0723@gmail.com>
2016-11-29 23:26:26 -0500

    count likes

--- client/views/respuestas/list/respuestas.js
+++ client/views/respuestas/list/respuestas.js
@@ -26,7 +26,7 @@ Template.Respuestas.events({
     'click .like': function(event){
         event.preventDefault();
         
-        var current_user_id = '1234568';
+        var current_user_id = '12345683';
         var current_event_id = event.currentTarget.id;
         params = {  id: current_event_id, 
                     params: {   $inc: { likes: 1 } ,  
