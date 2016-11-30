3effd23945779c7c96c0b26da0b555896739fa52
oskar771 <oscarpino711@gmail.com>
2016-11-30 04:37:19 -0500

    Add likes to respuestasForm

--- client/views/respuestas/list/respuestas.js
+++ client/views/respuestas/list/respuestas.js
@@ -5,7 +5,7 @@ Template.Respuestas.onCreated(function () {
     });
 });
 
-Template.Respuestas.helpers({
+Template.Respuestas.helpers({   
     respuestas:function () {
         return RespuestasForm.find({},{sort:{createdAt: -1}}).fetch();
     },
@@ -18,7 +18,28 @@ Template.Respuestas.helpers({
 
 Template.Respuestas.events({
 	'click .add-rta': function(event){
+        event.preventDefault();
 		console.log("add respuesta");
 		FlowRouter.go('respuesta');
-	}
+	},
+    
+    'click .like': function(event){
+        event.preventDefault();
+        
+        var current_user_id = '123456';
+        var current_event_id = event.currentTarget.id;
+        params = {  id: current_event_id, 
+                    params: {   $inc: { likes: 1 } ,  
+                                $push: { likers:  current_user_id } }
+                 };
+
+        var res = RespuestasForm.find( { _id: current_event_id}, { likers: 1}).fetch()[0].likers;
+        var q = _.find(res, (x) => x == current_user_id );
+        console.log('like', res, q); 
+        if ( q == current_user_id ) return;        
+        Meteor.call('RespuestasForm.update', params);
+
+        
+    }
+
 });
--- server/methods/respuestas.js
+++ server/methods/respuestas.js
@@ -3,6 +3,7 @@ Meteor.methods({
 		return RespuestasForm.insert(params);
 	},
 	'RespuestasForm.update': function(params){
+		console.log("respuestas: ", params);
 		return RespuestasForm.update({_id: params.id}, params.params);
 	},
 	'RespuestasForm.remove': function (params) {
