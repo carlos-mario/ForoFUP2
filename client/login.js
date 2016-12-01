0ab7c1ea6451c13fa0283ae26c19de6e42fd3bdc
carlos-mario <carlosluna0723@gmail.com>
2016-11-30 22:08:03 -0500

    update routes

--- client/login.js
+++ client/login.js
@@ -1,5 +1,5 @@
-Template.Login.helpers({
-    foros:function () {
-        return EditUsuario.find({usuarios},{sort:{createdAt: -1}}).fetch();
-    }
-});
+// Template.Login.helpers({
+//     foros:function () { 
+//         return EditUsuario.find({usuarios},{sort:{createdAt: -1}}).fetch();
+//     }
+// });
